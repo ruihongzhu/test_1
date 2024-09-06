8806e5d1d1e87a9751dba9ff918285c6b0344bba
mengxianghui.1998 <you@example.com>
2024-09-06 11:21:41 +0800

    a

--- functions/golang/cligo/main.go
+++ functions/golang/cligo/main.go
@@ -1,4 +1,4 @@
-package go01
+package cligo
 
 import (
 	"context"
@@ -25,7 +25,7 @@ type Result struct {
  */
 func Handler(ctx context.Context, params *Params) (*Result, error) {
 	// 日志功能
-	// 123application.GetLogger(ctx).Infof("%s 函数开始执行", time.Now().Format("2006-01-02 15:04:05.999"))
+	// application.GetLogger(ctx).Infof("%s 函数开始执行", time.Now().Format("2006-01-02 15:04:05.999"))
 
 	// 在这里补充业务代码
 
